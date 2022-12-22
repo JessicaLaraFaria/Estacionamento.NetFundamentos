fd5e9c192645bb3ff7446ea9bcaf10cb1887d828
Your Name <you@example.com>
2022-12-22 08:42:36 -0300

    ProjetoFinal

---
+++ Model/Estacionamento.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+
+namespace Model
+{
+    public class Estacionamento
+    {
+        private decimal precoInicial = 0;
+        private decimal precoPorHora = 0;
+        private List<string> veiculos = new List<string>();
+
+        public Estacionamento(decimal precoInicial, decimal precoPorHora)
+        {
+            this.precoInicial = precoInicial;
+            this.precoPorHora = precoPorHora;
+        }
+
+        public void AdicionarVeiculo()
+        {            
+            Console.WriteLine("Digite a placa do veiculo para estacionar:");
+            string placa = Console.ReadLine();
+            veiculos.Add(placa);
+        }
+
+        public void RemoverVeiculo()
+        {
+            Console.WriteLine("Digite a placa do veiculo para remover: ");
+            string placa = Console.ReadLine();
+
+            if (veiculos.Any(x => x.ToUpper() == placa.ToUpper()))
+            {
+                Console.WriteLine("Digite a quantidade de horas que o veiculo permaneceu no estacionamento: ");
+
+                int horas = int.Parse(Console.ReadLine());
+                decimal valorTotal = precoInicial + (precoPorHora * horas);
+                
+                Console.WriteLine($"O veiculo {placa} foi removido e o valor a ser recebido é : {valorTotal}");
+                veiculos.Remove(placa);
+            }
+            else
+            {
+                Console.WriteLine("Veiculo não encontrado. Confira os dados informados.");
+            }
+            Console.ReadKey();
+        }
+
+        public void ListarVeiculos()
+        {
+           if (veiculos.Any())
+           {
+               Console.WriteLine("Os veiculos estacionados são: ");
+
+               foreach(var item in veiculos)
+               {
+                    Console.WriteLine(item);
+               }
+           
+           }else
+           {
+               Console.WriteLine("Não há veiculos no estacionamento.");
+           }      
+           Console.ReadKey();
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using Model;
+
+namespace Principal
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            decimal precoInicial = 0;
+            decimal precoPorHora = 0;
+
+            Console.WriteLine("** Estacionamento **\n "+ "Digite o preço inicial: ");
+            precoInicial = Convert.ToDecimal(Console.ReadLine());
+
+            Console.WriteLine("Digite o preço a ser pago por hora: ");
+            precoPorHora = Convert.ToDecimal(Console.ReadLine());
+
+            Estacionamento estacionamento = new Estacionamento(precoInicial, precoPorHora);
+
+            string opcao = string.Empty;
+            bool exibirMenu = true;
+
+            while (exibirMenu)
+            {
+                Console.Clear();
+                Console.WriteLine("Escolha uma opção: ");
+                Console.WriteLine("1 - Cadastrar Veículo");
+                Console.WriteLine("2 - Remover Veículo");
+                Console.WriteLine("3 - Listar Veículo");
+                Console.WriteLine("4 - Encerrar");
+
+                switch (Console.ReadLine())
+                {
+                    case "1":
+                        estacionamento.AdicionarVeiculo();
+                        break;
+
+                    case "2":
+                        estacionamento.RemoverVeiculo();
+                        break;
+
+                    case "3":
+                        estacionamento.ListarVeiculos();
+                        break;
+
+                    case "4":
+                        exibirMenu = false;
+                        break;
+
+                    default:
+                        Console.WriteLine("Opção Inválida.");
+                        break;
+                }                
+                Console.WriteLine("Pressione uma tecla para continuar.");
+            }
+        }
+    }
+}
